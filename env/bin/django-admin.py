1e11d3a5651b5255e5a99dfbe3bdcd74d31e7a2f
Nicok78 <nicolas.kervoern@gmail.com>
2018-08-22 22:54:36 +0200

    remove env

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/nico/Documents/ESGI 2017_2018/PROJET ANNUEL/Projet_Annuel_ESGI_Server/Projet_Annuel_ESGI_API/env/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
